eab31c116a9495186a75ac6c98a78168650637b0
Scott <scott.donnelly790@gmail.com>
2018-02-28 16:19:04 -0500

    accordion body now scales with window height

--- src/Accordion/AccordionBody.js
+++ src/Accordion/AccordionBody.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import classes from './Accordion.css';
 
 class AccordionBody extends Component {
+  state = {
+    height: null,
+    width: null
+  }
+
+  componentDidMount() {
+    this.getHeight();
+    window.addEventListener('resize', () => {
+      this.getHeight();
+    });
+  }
+
+  getHeight() {
+    const height = this.accordionText.clientHeight;
+    this.setState({ height });
+  }
+
   render() {
-    const height = this.props.isActive ? { height: '160px' } : {}
+    const height = this.props.isActive ? { height: this.state.height } : {}
     return (
       <div className={`${classes['accordion-body']} container`} style={height}>
-        <p>{this.props.answer}</p>
+        <p ref={(accordionText) => { this.accordionText = accordionText }} className='py-2'>{this.props.answer}</p>
       </div>
     );
   }
