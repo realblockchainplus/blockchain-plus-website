bbc5c78587998060e73fade7875804cb1c066da3
Scott <scott.donnelly790@gmail.com>
2018-03-02 16:54:02 -0500

    temp nav links for routing testing

--- src/Header/NavList.js
+++ src/Header/NavList.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import NavListItem from './NavListItem';
+import { Link } from 'react-router-dom';
 
 class NavList extends Component {
   render() {
     return (
-      this.props.content.map((navItem, index) => {
-        const { name, id } = navItem;
-        return <NavListItem
-          key={index}
-          name={name}
-          id={id}
-          scrollToId={this.props.scrollToId}
-        />
-      })
+      <div>
+        {this.props.content.map((navItem, index) => {
+          const { name, id } = navItem;
+          return <NavListItem
+            key={index}
+            name={name}
+            id={id}
+            scrollToId={this.props.scrollToId}
+          />
+        })}
+        <Link to='/'>Home</Link>,
+        <Link to='/whitepaper'>Whitepaper</Link>
+      </div>
     );
   }
 }
