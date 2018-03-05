020218f7f7bfa34aefbf0011c98fb4a182695e13
Scott Donnelly <scott.donnelly790@gmail.com>
2018-03-04 22:37:09 -0500

    initial whitepaper page

--- src/Pages/Whitepaper/Whitepaper.js
+++ src/Pages/Whitepaper/Whitepaper.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import SectionHeader from '../../Section/SectionHeader';
 import classes from './Whitepaper.css';
 
 class Whitepaper extends Component {
   render() {
+    const sectionHeaderContent = { title: 'Our Whitepaper', desc: null };
     return (
-      <div className={`${classes['whitepaper']} container`} style={{ ...this.props.style }}>
-        aaaaaa
+      <div id={this.props.id} className={`${classes[this.props.id]} container text-center`}>
+        <div className={`${classes['whitepaper-container']}`}>
+          <SectionHeader content={sectionHeaderContent} backgroundColor={'rgba(242,254,254,1)'} />
+          <div className='row'>
+            <div className='col-8'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci sagittis eu volutpat odio facilisis mauris sit amet. Elementum pulvinar etiam non quam lacus suspendisse faucibus.
+            </div>
+            <div className='col-4'>
+              <img src='file.svg'/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
