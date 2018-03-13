24544974ab19c00ae285629f70f88426a92b9ed8
Scott <scott.donnelly790@gmail.com>
2018-03-13 11:18:30 -0400

    cleanup

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,36 +1,12 @@
 import React, { Component } from 'react';
-import { Router, Route, Switch, withRouter } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import MainPage from '../Pages/MainPage/MainPage';
 import Whitepaper from '../Pages/Whitepaper/Whitepaper';
-import Particles from 'react-particles-js';
-import { Parallax } from 'react-scroll-parallax';
 // import diff from 'deep-diff';
-import { AnimatedSwitch } from 'react-router-transition';
 // import Fade from '../Transitions/Fade';
 import classes from './Body.css';
 
 const duration = 300;
-
-const defaultStyle = {
-  transition: `all ${duration}ms`,
-	transformStyle: 'preserve-3d'
-};
-
-const transitionStyles = {
-  entering: { 
-    transform: 'rotateX(-90deg) translateZ(-50px)'
-  },
-  entered: {
-    transform: 'translateZ(50px)',
-  },
-  exiting: {
-    transform: 'rotateX(90deg) translateZ(50px)'
-  },
-  exited: {
-    transform: 'translateZ(-50px)',
-  },
-};
 
 class Body extends Component {
   componentDidMount() {
