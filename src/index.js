b482c61d6f51832eafef020a796815313bf3d4c3
Scott <scott.donnelly790@gmail.com>
2018-03-25 13:09:05 -0400

    enabled why-did-you-update for dev / test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import babel from 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppContainer from './AppContainer';
-import { ParallaxProvider } from 'react-scroll-parallax';
-import { BrowserRouter } from 'react-router-dom';
+// import { ParallaxProvider } from 'react-scroll-parallax';
+// import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
+// if (process.env.NODE_ENV !== 'production') {
+//     const {whyDidYouUpdate} = require('why-did-you-update')
+//     whyDidYouUpdate(React)
+// }
 const app = (
-  <AppContainer />
-  // <ParallaxProvider>
-  //   <BrowserRouter basename='blockchain-plus-website'>
-  //   </BrowserRouter>
-  // </ParallaxProvider>
+    <AppContainer />
+    /* <ParallaxProvider>
+     <BrowserRouter basename='blockchain-plus-website'>
+     </BrowserRouter>
+    </ParallaxProvider> */
 );
 
 ReactDOM.render(app, document.getElementById('root'));
