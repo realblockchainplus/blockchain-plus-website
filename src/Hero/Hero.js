1b3aa2743916e959cc4ac41f09a3c52254fff532
Scott <scott.donnelly790@gmail.com>
2018-04-02 10:16:42 -0400

    made hero image more responsive changed hero image (plus sign is different)

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import classes from './Hero.css'
 
 class Hero extends Component {
+  state = {
+    height: 0,
+    width: 0
+  }
   componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener('resize', () => {
+      this.updateDimensions();
+    })
+  }
+  updateDimensions() {
+    this.setState({
+      height: window.innerHeight,
+      width: window.innerWidth
+    });
   }
   render() {
+    console.log('[Hero] render called');
     return (
-      <div className={`${classes['hero-section']} col-12`} style={{ ...this.props.style }}>
+      <div className={`${classes['hero-section']} col-12`} style={{ ...this.state }}>
         <div style={{ color: 'white' }} className={`${classes['hero-container']} text-center`}>
-          {/* <svg id='svgout' /> */}
-          <img className={`${classes['hero-image']}`} src="logo_text.svg" alt='Blockchain Plus Text Logo' style={{ marginBottom: '10px' }} />
+          <img className={`${classes['hero-image']}`} src="logo_new_text_white_outline_2.svg" alt='Blockchain Plus Text Logo' style={{ marginBottom: '10px' }} />
           <h4 style={{ textTransform: 'uppercase' }}>{this.props.lang}</h4>
         </div>
       </div>
