a26b16f3982b1d6913db61cb8a5e0ae598f6883b
Scott <scott.donnelly790@gmail.com>
2018-04-02 10:15:29 -0400

    added encoding shim to reduce bundle size and make this section work on IE

--- src/RandomNumber/RandomNumberText.js
+++ src/RandomNumber/RandomNumberText.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './RandomNumber.css';
 import webCrypto from 'webcrypto-shim';
-import encoding from 'text-encoding';
 
 class RandomNumberText extends Component {
   state = {
@@ -10,6 +9,97 @@ class RandomNumberText extends Component {
   }
   componentDidMount() {
     this.generateRandomString((randomString) => { this.sha256(randomString) });
+  }
+  encode(str) {
+    var result;
+  
+    if ('undefined' === typeof Uint8Array) {
+      result = this.utf8ToBytes(str);
+    } else {
+      result = new Uint8Array(this.utf8ToBytes(str));
+    }
+  
+    return result;
+  }
+  utf8ToBytes(string, units) {
+    units = units || Infinity
+    var codePoint
+    var length = string.length
+    var leadSurrogate = null
+    var bytes = []
+    var i = 0
+  
+    for (; i < length; i++) {
+      codePoint = string.charCodeAt(i)
+  
+      // is surrogate component
+      if (codePoint > 0xD7FF && codePoint < 0xE000) {
+        // last char was a lead
+        if (leadSurrogate) {
+          // 2 leads in a row
+          if (codePoint < 0xDC00) {
+            if ((units -= 3) > -1) bytes.push(0xEF, 0xBF, 0xBD)
+            leadSurrogate = codePoint
+            continue
+          } else {
+            // valid surrogate pair
+            codePoint = leadSurrogate - 0xD800 << 10 | codePoint - 0xDC00 | 0x10000
+            leadSurrogate = null
+          }
+        } else {
+          // no lead yet
+  
+          if (codePoint > 0xDBFF) {
+            // unexpected trail
+            if ((units -= 3) > -1) bytes.push(0xEF, 0xBF, 0xBD)
+            continue
+          } else if (i + 1 === length) {
+            // unpaired lead
+            if ((units -= 3) > -1) bytes.push(0xEF, 0xBF, 0xBD)
+            continue
+          } else {
+            // valid lead
+            leadSurrogate = codePoint
+            continue
+          }
+        }
+      } else if (leadSurrogate) {
+        // valid bmp char, but last char was a lead
+        if ((units -= 3) > -1) bytes.push(0xEF, 0xBF, 0xBD)
+        leadSurrogate = null
+      }
+  
+      // encode utf8
+      if (codePoint < 0x80) {
+        if ((units -= 1) < 0) break
+        bytes.push(codePoint)
+      } else if (codePoint < 0x800) {
+        if ((units -= 2) < 0) break
+        bytes.push(
+          codePoint >> 0x6 | 0xC0,
+          codePoint & 0x3F | 0x80
+        )
+      } else if (codePoint < 0x10000) {
+        if ((units -= 3) < 0) break
+        bytes.push(
+          codePoint >> 0xC | 0xE0,
+          codePoint >> 0x6 & 0x3F | 0x80,
+          codePoint & 0x3F | 0x80
+        )
+      } else if (codePoint < 0x200000) {
+        if ((units -= 4) < 0) break
+        bytes.push(
+          codePoint >> 0x12 | 0xF0,
+          codePoint >> 0xC & 0x3F | 0x80,
+          codePoint >> 0x6 & 0x3F | 0x80,
+          codePoint & 0x3F | 0x80
+        )
+      } else {
+        throw new Error('Invalid code point')
+      }
+    }
+  
+    return bytes
   }
   hex(buffer) {
     const hexCodes = [];
@@ -26,7 +116,7 @@ class RandomNumberText extends Component {
     this.setState({ hashResult });
   }
   sha256(str) {
-    const buffer = new encoding.TextEncoder("utf-8").encode(str);
+    const buffer = this.encode(str);
     return crypto.subtle.digest("SHA-256", buffer).then((hash) => {
       return this.hex(hash);
     });
